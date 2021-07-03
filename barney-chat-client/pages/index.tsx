b18066d9b6aa7f8aae5f3a03e2930f933dd5de00
alexisveryreal <alexmaple20@gmail.com>
2021-07-02 19:59:32 -0700

    added chat testing

--- barney-chat-client/pages/index.tsx
+++ barney-chat-client/pages/index.tsx
@@ -7,6 +7,7 @@ interface Message {
 export default function Home() {
 
   const [messages, setMessages] = useState<Message[]>([])
+  const [text, setText] = useState("");
 
   const getMessages = useCallback(async () => {
     const res = await fetch('https://barney-chat-api.deno.dev/messages');
@@ -18,9 +19,26 @@ export default function Home() {
     getMessages();
   }, [])
 
+  const onSendMessage = useCallback(async () => {
+    await fetch('https://barney-chat-api.deno.dev/messages', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    });
+    setText("");
+    getMessages();
+  }, [text]);
+
   return (
     <div>
       {JSON.stringify(messages)}
+      </div><div>
+      <input type="text" value={text} onChange={(evt) => setText(evt.target.value)} />
+      <button onClick={onSendMessage}>Add</button>
     </div>
   );
 }
